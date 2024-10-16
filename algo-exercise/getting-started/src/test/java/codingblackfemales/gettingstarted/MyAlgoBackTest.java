9fea8035c33653ae55ed5e792b204e8c58470c9e
Nyadej <naomiadejumo@gmail.com>
2024-10-16 19:02:32 +0100

    added test asserts to check filled quantity and total order limit

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -1,7 +1,11 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MyAlgoBackTest extends AbstractAlgoBackTest {
 
@@ -22,18 +26,20 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         send(createTickBuy3());
         send(createTickBuy4());
 
-        //ADD asserts when you have implemented your algo logic
-        //assertEquals(container.getState().getChildOrders().size(), 3);
-
         //when: market data moves towards us
 
         //then: get the state
         var state = container.getState();
 
-        //Check things like filled quantity, cancelled order count etc....
-        //long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
-        //and: check that our algo state was updated to reflect our fills when the market data
-        //assertEquals(225, filledQuantity);
+        // Check the total filled quantity
+        long totalFilledQuantity = state.getChildOrders().stream()
+                .mapToLong(ChildOrder::getFilledQuantity)
+                .sum();
+
+        assertEquals("Total filled quantity is 3500", 3500, totalFilledQuantity);
+
+        // Ensure no more than the total order limit is reached
+        assertTrue("Total orders should be 5", state.getChildOrders().size() <= 5);
     }
 
 }
